0740b2523bd48c3ea388e4b6498ee11e7d38b11c
Thiago-DR <thiago.dr.1055@gmail.com>
2022-10-13 10:23:11 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-purple-900'>Hello World</h1>
+
+      <button className='bg-purple-900 font-medium px-8 py-3 rounded text-white hover:bg-purple-700'>
+        Botao
+      </button>
+    </>
+  )
+}
+
